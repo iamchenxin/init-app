eb7393f83092311fc2a4be70b81faf06444c93c9
iamchenxin <iamchenxin@gmail.com>
2016-10-13 06:56:50 -0600

    clean unused tasks

--- packages/config-react/gulpfile.js
+++ packages/config-react/gulpfile.js
@@ -1,26 +1,11 @@
 // @flow
 /*eslint-env node */
 const gulp = require('gulp');
-const base = require('./config/base.js');
+const { ports} = require('./config/base.js');
 const scripts = require('init-scripts');
 const hotConfig = require('./config/webpack.hot.js');
-const { utils } = scripts;
-
-gulp.task('lib', function() {
-  const babelJson = utils.readRC(base.paths.babelrc);
-  return scripts.gulpscripts.compileJS('src', 'lib', babelJson);
-});
-
-gulp.task('clean', function() {
-  return utils.rmdir([
-    'lib',
-    'index.js',
-    'index.js.flow',
-    'index.js.map',
-  ] );
-});
 
 gulp.task('hot', () => {
   const compiler = new scripts.webpackscripts.WebCompiler(hotConfig);
-  return compiler.HotServer(base.ports.web);
+  return compiler.HotServer(ports.web);
 });
