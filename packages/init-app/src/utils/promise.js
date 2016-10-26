b004202066bf82b6ff5f844fec9c5f9c654830b0
iamchenxin <iamchenxin@gmail.com>
2016-10-26 07:29:46 -0600

    promisify always resolve results as an Array

--- packages/init-app/src/utils/promise.js
+++ packages/init-app/src/utils/promise.js
@@ -6,15 +6,12 @@ export function wait(delay: number): Promise<void> {
   });
 }
 
+// result alway be an Array [a,b,c]
 export function promisify(fn: Function, firstData?: boolean): () => Promise<any> {
   return function(...args): Promise<any> {
     return new Promise(function(resolve, reject) {
       args.push(function(err, ...result) {
         let res = result;
-
-        if (result.length <= 1) {
-          res = result[0];
-        }
 
         if (firstData) {
           res = err;
